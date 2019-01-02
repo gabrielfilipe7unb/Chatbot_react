a14da44d1a1260d5139385cdc21a5be9b178482c
Gabriel Filipe <gabrielfilipe7unb@gmail.com>
2019-01-02 19:32:41 -0200

    Adds a button on the chat layout

--- src/Componentes/chatbot/ChatMensagem.js
+++ src/Componentes/chatbot/ChatMensagem.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import {InputGroup, Input, InputGroupAddon, Button} from 'reactstrap'
 
 const ChatMensagem = porps => {
     return (
         <div className='chat-mensagem'>
-            Digite aqui sua mensagem
+        <InputGroup>
+            <Input placeholder='Digite aqui sua mensagem' />
+            <InputGroupAddon addonType='append'>
+                <Button color='success'>Enviar</Button>
+            </InputGroupAddon>
+        </InputGroup>
         </div>
     )
 }
