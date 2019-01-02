cc8d4a257b527f20663b994ff9bb508da8e239fa
Gabriel Filipe <gabrielfilipe7unb@gmail.com>
2019-01-02 13:19:17 -0200

    Configures the Home to the unique file

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './Componentes/Header';
 import Footer from './Componentes/Footer';
-import { Button } from 'reactstrap';
+import Home from './Componentes/Home';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <Button color="danger">Danger!</Button>
+        <Home />
         <Footer />
       </div>
     );
---
+++ src/Componentes/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Button } from 'reactstrap';
+
+class Home extends Component {
+    render(){
+        return (
+            <Button color="danger">Danger!</Button>
+        )
+    }
+}
+
+export default Home
